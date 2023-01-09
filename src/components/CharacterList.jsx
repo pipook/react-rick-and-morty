f75ff0512b77182530ef945a3957d0f1d28812eb
Daniel Chico P <pipook@gmail.com>
2023-01-09 14:50:32 -0500

    React Vite app Rick And Morty API first commit

---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import Character from "./Character";
+import NavPage from "./NavPage";
+
+function CharacterList() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${page}`
+      );
+      const data = await response.json();
+      setLoading(false);
+      setCharacters(data.results);
+    }
+    fetchData();
+  }, [page]);
+  return (
+    <div className="container">
+      <NavPage page={page} setPage={setPage} />
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="row">
+          {characters.map((character) => {
+            return (
+              <div key={character.id} className="col-md-4">
+                <Character character={character} />
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <NavPage page={page} setPage={setPage} />
+    </div>
+  );
+}
+
+export default CharacterList;
