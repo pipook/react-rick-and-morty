75bf8f615889e073777860154551564e4efa8189
Daniel Chico P <pipook@gmail.com>
2023-01-09 16:46:05 -0500

    fix last page count

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -6,6 +6,7 @@ function CharacterList() {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(0)
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(
@@ -14,12 +15,18 @@ function CharacterList() {
       const data = await response.json();
       setLoading(false);
       setCharacters(data.results);
+      if(lastPage === 0){
+        lastesPage(data.info.pages);
+      }
     }
     fetchData();
   }, [page]);
+  const lastesPage = page => {
+    setLastPage(page);
+  }
   return (
     <div className="container">
-      <NavPage page={page} setPage={setPage} />
+      <NavPage page={page} setPage={setPage} lastPage={lastPage} />
       {loading ? (
         <h1>Loading...</h1>
       ) : (
--- src/components/NavPage.jsx
+++ src/components/NavPage.jsx
@@ -1,13 +1,17 @@
-function NavPage({ page, setPage }) {
+function NavPage({ page, setPage, lastPage }) {
   return (
     <header className="d-flex justify-content-between align-items-center">
       <p>Page: {page}</p>
-      <button
-        className="btn btn-primary btn-sm"
-        onClick={() => setPage(page + 1)}
-      >
-        Page {page + 1}
-      </button>
+      {page + 1 > lastPage ? (
+        <p>Last page.</p>
+      ) : (
+        <button
+          className="btn btn-primary btn-sm"
+          onClick={() => setPage(page + 1)}
+        >
+          Page {page + 1}
+        </button>
+      )}
     </header>
   );
 }
